d5a44cbdbbf729b5351622646bed3ddf3af8b100
IvanCaceresS <icaceressatorres2001@gmail.com>
2025-11-06 20:49:19 -0300

    case-insensitive

--- flappy-cat/server/src/score/dto/create-score.dto.ts
+++ flappy-cat/server/src/score/dto/create-score.dto.ts
@@ -4,7 +4,7 @@ export class CreateScoreDto {
   
   @IsNotEmpty()
   @Length(3, 5)
-  @Matches(/^[A-Z0-9Ñ]+$/, {
+  @Matches(/^[A-Z0-9Ñ]+$/i, {
     message: 'El nombre solo puede contener letras (A-Z, Ñ) y números (0-9)',
   })
   name: string;
